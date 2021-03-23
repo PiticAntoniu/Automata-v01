b19433d61ed22570bbf013745bacca03050ebce7
antoniu.pitic <antoniu.pitic@gmail.com>
2021-03-23 13:17:12 +0200

    - MochData method deleted - added default Automata to Project

--- Automata v01/Automata.cs
+++ Automata v01/Automata.cs
@@ -19,68 +19,6 @@ namespace Automata_v01
         public List<Tranzitie> F { get => f; set => f = value; }
 
         #endregion
-        internal void MockData()
-        {
-            I.AddRange(new List<char> { 'a', 'b', 'c' });
-            S.AddRange(new List<string> { "q0","q1","q2"});
-            Sf.Add("q2");
-
-            F.Add(new Tranzitie
-            {
-                PerecheDeIntrare = ("q0", 'a'),
-                StareDeIesire = "q0"
-            });
-
-            F.Add(new Tranzitie
-            {
-                PerecheDeIntrare = ("q0", 'b'),
-                StareDeIesire = "q1"
-            });
-
-            F.Add(new Tranzitie
-            {
-                PerecheDeIntrare = ("q0", 'c'),
-                StareDeIesire = "q1"
-            });
-
-            F.Add(new Tranzitie
-            {
-                PerecheDeIntrare = ("q1", 'a'),
-                StareDeIesire = "q1"
-            });
-
-            F.Add(new Tranzitie
-            {
-                PerecheDeIntrare = ("q1", 'b'),
-                StareDeIesire = "q1"
-            });
-
-            F.Add(new Tranzitie
-            {
-                PerecheDeIntrare = ("q1", 'c'),
-                StareDeIesire = "q2"
-            });
-
-
-            F.Add(new Tranzitie
-            {
-                PerecheDeIntrare = ("q2", 'a'),
-                StareDeIesire = "q2"
-            });
-
-            F.Add(new Tranzitie
-            {
-                PerecheDeIntrare = ("q2", 'b'),
-                StareDeIesire = "q2"
-            });
-
-            F.Add(new Tranzitie
-            {
-                PerecheDeIntrare = ("q2", 'c'),
-                StareDeIesire = "q2"
-            });
-        }
-
      
     }
 }
